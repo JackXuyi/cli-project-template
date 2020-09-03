63444bc10b4b3f6df96b322a85e2cb1ca6d2a042
许一 <xuyi@tuya.com>
2020-09-03 20:29:54 +0800

    feat: path 路径处理

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,4 +1,5 @@
 import * as express from 'express'
+import * as path from 'path'
 
 import api from './api'
 
@@ -9,5 +10,10 @@ router.get('/', (req, res) => {
 })
 
 router.use('/api', api)
+
+// 未匹配到的路由都返回这个页面，path 路径模式
+router.get('/*', function (req, res) {
+  res.sendFile(path.resolve(__dirname, '../../tsconfig.json'))
+})
 
 export default router
